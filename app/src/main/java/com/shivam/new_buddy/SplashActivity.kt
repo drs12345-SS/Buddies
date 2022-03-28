bf7d1a3eff2a2fdfe28c150a2544bacaf10b70fa
ryan <arunmaurya2500@gmail.com>
2022-03-29 00:42:53 +0530

    splash screen done

--- app/src/main/java/com/shivam/new_buddy/SplashActivity.kt
+++ app/src/main/java/com/shivam/new_buddy/SplashActivity.kt
@@ -4,11 +4,26 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import android.view.Window
+import android.view.WindowManager
+import com.shivam.new_buddy.databinding.ActivitySplashBinding
 
-class MainActivity : AppCompatActivity() {
+class SplashActivity : AppCompatActivity() {
+
+    lateinit var binding : ActivitySplashBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivitySplashBinding.inflate(layoutInflater)
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
+
+        setContentView(binding.root)
+
         Handler().postDelayed(Runnable { //This method will be executed once the timer is over
             // Start your app main activity
             val i = Intent(this, DashboardActivity::class.java)
